19eb35521d69ea7979b212bf37fbd3f70878c14f
varsha <varshaprajapati.15004@gmail.com>
2022-08-13 19:53:49 +0530

    Updates

---
+++ precache-manifest.40b16905d654ad03b0047798be13d5d5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4356e93975083093afb8635ca1612aa",
+    "url": "/Emojipedia/index.html"
+  },
+  {
+    "revision": "3024233d997c41ba05e2",
+    "url": "/Emojipedia/static/js/2.4ab6b1c1.chunk.js"
+  },
+  {
+    "revision": "1a0b84b4aee12c3dd1cf",
+    "url": "/Emojipedia/static/js/main.1122c9c3.chunk.js"
+  },
+  {
+    "revision": "6a0fb8173f6d7a6f503f",
+    "url": "/Emojipedia/static/js/runtime-main.9315cabc.js"
+  }
+]);
